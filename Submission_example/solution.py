0dda13e2cbda4d257c2197e86c6fe4d5bbefcdbc
Ekaterina <ktrofimova@protonmail.com>
2019-06-14 15:37:02 +0300

    Update solution.py

--- Submission_example/solution.py
+++ Submission_example/solution.py
@@ -10,7 +10,7 @@ def main():
     df = np.loadtxt(input_dir + '/data.data')
     df = pd.DataFrame(df, columns=['column 1', 'column 2'])
     df['result'] = df['column 1'] + df['column 2']
-    np.savetxt(output_dir + '/data.prediction', np.array(df['result']))
+    np.savetxt(output_dir + '/data.predict', np.array(df['result']))
     return 0
 
 if __name__ == "__main__":
