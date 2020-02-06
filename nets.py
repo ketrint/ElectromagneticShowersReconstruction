01787f7542299e589cdb1255bc7e09188ad45971
Ekaterina <ktrofimova@protonmail.com>
2020-02-06 14:05:35 +0300

    Update nets.py

--- nets.py
+++ nets.py
@@ -13,23 +13,28 @@ from torch_geometric.nn import PointConv, EdgeConv, SplineConv
 
 
 class EmulsionConv(MessagePassing):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, edge_dim):
         super().__init__(aggr='add')
-        self.mp = torch.nn.Linear(in_channels * 2, out_channels)
+        self.mp = torch.nn.Linear(in_channels * 2 + edge_dim, out_channels)
 
-    def forward(self, x, edge_index, orders):
+    def forward(self, x, edge_index, orders, dist):
         for order in orders:
-            x = self.propagate(torch.index_select(edge_index[:, order],
-                                                  0,
-                                                  torch.LongTensor([1, 0]).to(x.device)), x=x)
+            x = self.propagate(
+                torch.index_select(
+                    edge_index[:, order],
+                    0,
+                    torch.LongTensor([1, 0]).to(x.device)
+                ), 
+                x=x,
+                dist=dist[order]
+            )
         return x
 
-    def message(self, x_j, x_i):
-        return self.mp(torch.cat([x_i, x_j - x_i], dim=1))
+    def message(self, x_j, x_i, dist):
+        return self.mp(torch.cat([x_i, x_j - x_i, dist], dim=1))
 
     def update(self, aggr_out, x):
         return aggr_out + x
-
 
 class GraphNN_KNN_v1(nn.Module):
     def __init__(self, k, dim_out=10):
@@ -163,19 +168,19 @@ class GraphNN_KNN_v3(nn.Module):
         self.emconv1 = EmulsionConv(self.k, self.k)
         self.emconv2 = EmulsionConv(self.k, self.k)
         self.emconv3 = EmulsionConv(self.k, self.k)
-        self.wconv1 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.wconv2 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
-        self.wconv3 = EdgeConv(Sequential(nn.Linear(20, 10)), 'max')
+        self.wconv1 = EdgeConv(Sequential(nn.Linear(21, 10)), 'max')
+        self.wconv2 = EdgeConv(Sequential(nn.Linear(21, 10)), 'max')
+        self.wconv3 = EdgeConv(Sequential(nn.Linear(21, 10)), 'max')
         self.output = nn.Linear(10, dim_out)
 
     def forward(self, data):
-        x, edge_index, orders = data.x, data.edge_index, data.mask
-        x = self.emconv1(x=x, edge_index=edge_index, orders=orders)
-        x = self.emconv2(x=x, edge_index=edge_index, orders=orders)
-        x = self.emconv3(x=x, edge_index=edge_index, orders=orders)
-        x1 = self.wconv1(x=x, edge_index=edge_index)
-        x2 = self.wconv2(x=x1, edge_index=edge_index)
-        x3 = self.wconv3(x=x2, edge_index=edge_index)
+        x, edge_index, orders, dist = data.x, data.edge_index, data.mask, data.edge_attr
+        x = self.emconv1(x=x, edge_index=edge_index, orders=orders, dist=dist)
+        x = self.emconv2(x=x, edge_index=edge_index, orders=orders, dist=dist)
+        x = self.emconv3(x=x, edge_index=edge_index, orders=orders, dist=dist)
+        x1 = self.wconv1(x=x, edge_index=edge_index, dist=dist)
+        x2 = self.wconv2(x=x1, edge_index=edge_index, dist=dist)
+        x3 = self.wconv3(x=x2, edge_index=edge_index, dist=dist)
      
         return self.output(x3)
     
